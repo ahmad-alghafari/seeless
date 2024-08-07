f09d42ecd91f5a4abf17e380e77b15ec360e0642
ahmad-alghafari <hxism7.gh@gmail.com>
2024-08-07 22:54:34 +0300

    modfi cart

--- resources/js/components/foodList.vue
+++ resources/js/components/foodList.vue
@@ -2,28 +2,66 @@
 import {defineProps ,onMounted , ref , reactive } from 'vue';
 
 const props = defineProps({
-  categories: Array, // Ensure categories is defined as an Array
+  categories: Object, // Ensure categories is defined as an Array
   food: Object,
 });
+
+
 const cart = ref([]);
 
-const addToCart = (id) => {
-  if (cart.value[id]) {
-    cart.value[id]++;
-    } else {
-    cart.value[id] = 1;
-  }
-  console.log("cart = " + cart.value[id]);
+const printCart = () => {
+  console.log("Cart Contents:");
+  for (const id in cart.value) {
+    if (cart.value.hasOwnProperty(id)) {
+      console.log(`ID: ${id}, Quantity: ${cart.value[id]}`);
+    }
+  } 
 }
 
-const Cart = (id) => {
+const add_increase = (id) => {
   if (cart.value[id]) {
     cart.value[id]++;
     } else {
     cart.value[id] = 1;
   }
-  console.log("cart = " + cart.value[id]);
+  printCart();
 }
+
+const decreaseQuantity = (id) => {
+  if(cart.value[id] > 1 ){
+    cart.value[id]--;
+  }else if(cart.value[id] == 1){
+    delete cart.value[id];
+  }
+  printCart();
+}
+
+const deleteAllquantity = (id) => {
+  delete cart.value[id];
+  printCart();
+}
+
+const isAdded = (id) => {
+  if(cart.value[id]){
+    return true ;
+  }else{
+    return false;
+  }
+}
+
+const isEmpty = () => {
+  if(cart.value.length == 0){ 
+    return true ;
+  }else{
+    return false;
+  }
+}
+
+
+
+
+
+
 </script>
 <template>
   <section class="food_section layout_padding">
@@ -37,7 +75,7 @@ const Cart = (id) => {
 
       <ul class="filters_menu">
         <li class="active" data-filter="*">All</li>
-        <li v-for="cat in props.categories" :key="cat.id" :data-filter="`.${cat}`">{{ cat }}</li>
+        <li v-for="cat in props.categories" :key="cat.index" :data-filter="`.${cat}`">{{ cat }}</li>
       </ul>
 
       <div class="filters-content">
@@ -57,69 +95,28 @@ const Cart = (id) => {
                     {{ fod.description }}
                   </p>
                   <div class="options">
+                    {{ fod.availability }}
+                  </div>
+                  <div >
                     <h6>
                       {{ fod.price }} SR
                     </h6>
-                    <button type="button" v-on:click="addToCart(index)" >
-                      <!-- <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
-                        <g>
-                          <g>
-                            <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248
-                         c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
-                          </g>
-                        </g>
-                        <g>
-                          <g>
-                            <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48
-                         C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064
-                         c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4
-                         C457.728,97.71,450.56,86.958,439.296,84.91z" />
-                          </g>
-                        </g>
-                        <g>
-                          <g>
-                            <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296
-                         c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
-                          </g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                        <g>
-                        </g>
-                      </svg> -->
+                    <button type="button" @click="add_increase(index)" >
+                      
                     add to cart
+                    </button>
+                    <button type="button" @click="add_increase(index)" >
+                      increase 
+                    </button>
+                    <button type="button" @click="deleteAllquantity(index)" >
+                      delete 
+                    </button>
+                    <button type="button" @click="decreaseQuantity(index)" >
+                      decrease
                     </button>
                     
                   </div>
-                  <div class="options">
-                    {{ fod.availability }}
-                  </div>
+                  
                 </div>
               </div>
             </div>
@@ -129,4 +126,75 @@ const Cart = (id) => {
       </div>
     </div>
   </section>  
-  </template>
+  <button  type="button" class="floating-button btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Cart</button>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Cart Contents</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          
+          <table  class="table table-striped">
+            <thead>
+              <tr>
+                <th>test</th>
+                <th>Food Name</th>
+                <th>Number</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="id in cart" :key="id">
+                <td>{{ id }}</td>
+                <td>{{ props.food[id].name }}</td>
+                <td>{{ cart[id] }}</td>
+                <td>{{ props.food[id].price }}</td>
+                <td>{{ cart[id] * props.food[id].price }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="delete">Delete Contents</button>
+          <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style>
+.floating-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 60px;
+  height: 60px;
+  background-color: #f39c12;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  font-size: 24px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+@media (max-width: 600px) {
+  .floating-button {
+    width: 50px;
+    height: 50px;
+    font-size: 20px;
+    bottom: 15px;
+    right: 15px;
+  }
+}
+</style>
